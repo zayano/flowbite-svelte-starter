a283e00fe76549865d10edcde85cf7bd5656393b
Reza Aditya Sutrisno <reza.aditya@pelni.co.id>
2025-08-13 11:30:40 +0700

    add change password

--- src/components/ChangePasswordForm.svelte
+++ src/components/ChangePasswordForm.svelte
@@ -3,14 +3,14 @@
 	import { Button, Checkbox, Label, Input } from 'flowbite-svelte';
 </script>
 
-<Section name="login">
+<Section name="reset">
 	<Register href="/">
 		{#snippet top()}
 			<img class="h-20 w-40" src="/images/logo/pelni-logo.png" alt="logo" />
 		{/snippet}
 		<div class="space-y-4 p-6 sm:p-8 md:space-y-6">
 			<form class="flex flex-col space-y-6" action="/">
-				<h3 class="p-0 text-xl font-medium text-gray-900 dark:text-white">Login</h3>
+				<h3 class="p-0 text-xl font-medium text-gray-900 dark:text-white">Change Password</h3>
 				<Label class="space-y-2">
 					<span>Your email</span>
 					<Input type="email" name="email" placeholder="name@company.com" required />
@@ -19,21 +19,19 @@
 					<span>Your password</span>
 					<Input type="password" name="password" placeholder="•••••" required />
 				</Label>
+				<Label class="space-y-2">
+					<span>Confirm password</span>
+					<Input type="password" name="confirm-password" placeholder="•••••" required />
+				</Label>
 				<div class="flex items-start">
-					<Checkbox>Remember me</Checkbox>
-					<a
-						href="/change_password"
-						class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
-						>Forgot password?</a
-					>
+					<Checkbox>
+						I accept the <a
+							class="text-primary-600 dark:text-primary-500 font-medium hover:underline"
+							href="/">Terms and Conditions</a
+						>
+					</Checkbox>
 				</div>
-				<Button type="submit" class="w-full1">Sign in</Button>
-				<p class="text-sm font-light text-gray-500 dark:text-gray-400">
-					Don’t have an account yet? <a
-						href="/register"
-						class="text-primary-600 dark:text-primary-500 font-medium hover:underline">Sign up</a
-					>
-				</p>
+				<Button type="submit" class="w-full1">Reset passwod</Button>
 			</form>
 		</div>
 	</Register>
---
+++ src/routes/change_password/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import ChangePasswordForm from '../../components/ChangePasswordForm.svelte';
+</script>
+
+<div class="change-password-container">
+	<ChangePasswordForm />
+</div>
+
+<style>
+	.change-password-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		min-height: 100vh;
+		width: 100%;
+	}
+</style>

2b23efb5e3c953518f8397f01b88360257893a97
Reza Aditya Sutrisno <reza.aditya@pelni.co.id>
2025-08-13 11:25:31 +0700

    update home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
-  import { Alert } from 'flowbite-svelte';
+	import { Alert, Button } from 'flowbite-svelte';
 </script>
 
 <div class="p-8">
 	<h1 class="my-8">Flowbite Svelte Starter</h1>
-  <Alert>
-    <span class="font-medium">Alert example</span>
-    Change a few things up and try submitting again.
-  </Alert>
+	<Alert>
+		<span class="font-medium">Alert example</span>
+		Change a few things up and try submitting again.
+	</Alert>
+
+	<Button class="mt-10" href="/login">LOGIN</Button>
 </div>

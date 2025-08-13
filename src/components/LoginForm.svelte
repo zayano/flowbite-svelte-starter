b07562b37ae2a05cb46e6e44e255214fb73029ce
Reza Aditya Sutrisno <reza.aditya@pelni.co.id>
2025-08-13 11:24:40 +0700

    add login form

---
+++ src/components/LoginForm.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Section, Register } from 'flowbite-svelte-blocks';
+	import { Button, Checkbox, Label, Input } from 'flowbite-svelte';
+</script>
+
+<Section name="login">
+	<Register href="/">
+		{#snippet top()}
+			<img class="h-20 w-40" src="/images/logo/pelni-logo.png" alt="logo" />
+		{/snippet}
+		<div class="space-y-4 p-6 sm:p-8 md:space-y-6">
+			<form class="flex flex-col space-y-6" action="/">
+				<h3 class="p-0 text-xl font-medium text-gray-900 dark:text-white">Login</h3>
+				<Label class="space-y-2">
+					<span>Your email</span>
+					<Input type="email" name="email" placeholder="name@company.com" required />
+				</Label>
+				<Label class="space-y-2">
+					<span>Your password</span>
+					<Input type="password" name="password" placeholder="•••••" required />
+				</Label>
+				<div class="flex items-start">
+					<Checkbox>Remember me</Checkbox>
+					<a href="/" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
+						>Forgot password?</a
+					>
+				</div>
+				<Button type="submit" class="w-full1">Sign in</Button>
+				<p class="text-sm font-light text-gray-500 dark:text-gray-400">
+					Don’t have an account yet? <a
+						href="/register"
+						class="text-primary-600 dark:text-primary-500 font-medium hover:underline">Sign up</a
+					>
+				</p>
+			</form>
+		</div>
+	</Register>
+</Section>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import LoginForm from '../../components/LoginForm.svelte';
+</script>
+
+<div class="login-container">
+	<LoginForm />
+</div>
+
+<style>
+	.login-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		min-height: 100vh;
+		width: 100%;
+	}
+</style>
